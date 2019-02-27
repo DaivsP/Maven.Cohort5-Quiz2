4bca1b7c25deff7dd354c2762475debe3f4a3609
Davis Perchik <davisperchik@gmail.com>
2019-02-27 14:13:36 -0500

    integerarrayutilites works

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,19 +1,35 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        Boolean answer = false;
+        Integer arrayLength = array.length;
+        if (arrayLength % 2 == 0){
+            answer = true;
+        }
+        return answer;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        List<Integer> answer = new ArrayList<>();
+        for (Integer i = start; i <= stop; i++) {
+            answer.add(i);
+        }
+        return answer.toArray(new Integer[0]);
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+        Integer firstNumber = array[0];
+        Integer secondNumber = array[1];
+        return firstNumber+secondNumber;
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        Integer firstNumber = array[0];
+        Integer secondNumber = array[1];
+        return firstNumber*secondNumber;
     }
 }
