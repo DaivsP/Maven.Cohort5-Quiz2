77f113f9e324614cc7a2e71cf392a0210c6ce2ae
Davis Perchik <davisperchik@gmail.com>
2019-02-27 15:11:53 -0500

    ArrayUtility tests work

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,19 +1,65 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.*;
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.addAll(Arrays.asList(array1));
+        arrayList.addAll(Arrays.asList(array2));
+        return arrayList.toArray(new Integer[0]);
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
-        return null;
+        Integer[] newArray = new Integer[array.length];
+        for(int i = 0; i < array.length; i++){
+            newArray[(i+(array.length-index)) % array.length ] = array[i];
+        }
+        return newArray;
     }
 
     public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
-        return null;
+        List<Integer> listArray1 = new ArrayList<>();
+        List<Integer> listArray2 = new ArrayList<>();
+        for (Integer integer: array1) {
+            listArray1.add(integer);
+        }
+        for (Integer integer: array2) {
+            listArray2.add(integer);
+        }
+        Integer counter = 0;
+        for (int i = 0; i < listArray1.size(); i++) {
+            if (listArray1.get(i) == valueToEvaluate){
+                counter++;
+            }
+        }
+        for (int i = 0; i < listArray2.size(); i++) {
+            if (listArray2.get(i) == valueToEvaluate){
+                counter++;
+            }
+        }
+        return counter;
     }
 
     public Integer mostCommon(Integer[] array) {
-        return null;
+        int count = 1, tempCount;
+        int popular = array[0];
+        int temp = 0;
+        for (int i = 0; i < (array.length - 1); i++)
+        {
+            temp = array[i];
+            tempCount = 0;
+            for (int j = 1; j < array.length; j++)
+            {
+                if (temp == array[j])
+                    tempCount++;
+            }
+            if (tempCount > count)
+            {
+                popular = temp;
+                count = tempCount;
+            }
+        }
+        return popular;
     }
 }
