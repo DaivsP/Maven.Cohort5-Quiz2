7e90ad0d22d6067a4a9c55084a83382e9f7e8838
Davis Perchik <davisperchik@gmail.com>
2019-02-27 16:15:29 -0500

    Part3 tests done

--- src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
@@ -1,4 +1,24 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class RedRobin {
+public class RedRobin extends Bird{
+    String migrationMonth;
+
+    @Override
+    public int getSpeed() {
+        return 10;
+    }
+
+    @Override
+    public String color() {
+        return "red";
+    }
+
+    public String getMigrationMonth(){
+        return this.migrationMonth;
+    }
+
+    @Override
+    public void setMigrationMonth(String migrationMonth) {
+        this.migrationMonth = migrationMonth;
+    }
 }
