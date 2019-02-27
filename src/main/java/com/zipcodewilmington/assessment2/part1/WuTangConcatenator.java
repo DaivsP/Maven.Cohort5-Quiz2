e143f897dc0eb1156b7a89e7a054e4593aff4b8f
Davis Perchik <davisperchik@gmail.com>
2019-02-27 14:24:06 -0500

    WuTang done, part 1 100% done

--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -1,18 +1,33 @@
 package com.zipcodewilmington.assessment2.part1;
 
+
 public class WuTangConcatenator {
+    public Integer input;
     public WuTangConcatenator(Integer input) {
+        this.input = input;
     }
 
     public Boolean isWu() {
-        return null;
+        Boolean answer = false;
+        if (input % 3 == 0){
+            answer = true;
+        }
+        return answer;
     }
 
     public Boolean isTang() {
-        return null;
+        Boolean answer = false;
+        if (input % 5 == 0){
+            answer = true;
+        }
+        return answer;
     }
 
     public Boolean isWuTang() {
-        return null;
+        Boolean answer = false;
+        if (input % 3 == 0 && input % 5 == 0){
+            answer = true;
+        }
+        return answer;
     }
 }
